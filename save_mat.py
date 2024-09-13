3819f53932731dbd21ad8bbb4fe943b87f317826
wwg1010 <32235000+wwg1010@users.noreply.github.com>
2024-09-14 07:40:02 +0800

    Update save_mat.py

--- save_mat.py
+++ save_mat.py
@@ -3,27 +3,7 @@ import scipy.io as scio
 import torch
 import numpy as np
 
-
-# def Save_mat(self , query_img , retrieval_img , mode_name="i2t"):
-#     save_dir = os.path.join(self.args.save_dir , "PR_cruve")
-#     os.makedirs(save_dir,exist_ok=True)
-#
-#     query_img = query_img.cpu().detach().numpy()
-#     retrieval_img = retrieval_img.cpu().detach().numpy()
-#
-#     query_label = self.query_labels.numpy()
-#     retrieval_label = self.retrieval_labels.numpy()
-#
-#     result_dict = {
-#         'q_img' : query_img ,
-#         'r_img' : retrieval_img ,
-#         'q_l' : query_label ,
-#         'r_l' : retrieval_label
-#     }
-#
-#     scio.savemat(os.path.join(save_dir , str(self.args.ouput_dim)
-#                  + "-ours-" + self.args.datasets + "-" , + mode_name + ".mat"),result_dict)
-def Save_mat(epoch , output_dim , datasets , query_labels , retrieval_labels , query_img , retrieval_img , save_dir='..' , mode_name="DSH",map=0):
+def Save_mat(epoch , output_dim , datasets , query_labels , retrieval_labels , query_img , retrieval_img , save_dir='..' , mode_name="CrossHash",map=0):
     '''
     save_dir: 保存文件的目录路径
     output_dim: 输出维度
